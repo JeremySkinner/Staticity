b9b81ff9cdb759aaf675a0a7a6bc2cab8332f6ca
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2011-08-19 14:22:18 +0100

    Initial work on syntax highlighting thanks to Phalanger and Geshi

---
+++ src/Staticity.Tests/SyntaxHighlightTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Staticity.Tests {
+	using System;
+	using System.IO;
+	using NUnit.Framework;
+	using PHP.Core;
+	using PHP.Library;
+
+	[TestFixture]
+	public class SyntaxHighlightTest {
+		[Test]
+		public void Highlights_csharp_code() {
+			var code = "class Foo { int Bar() { } }";
+
+			var highlighter = new SyntaxHighlighter();
+			var result = highlighter.Highlight(code, "csharp");
+			Console.WriteLine(result);
+
+			StringAssert.StartsWith("<pre class=\"csharp\" style=\"font-family:monospace;\"><span style=\"color: #FF0000;\">class</span>", result);
+		}
+	}
+}
---
+++ src/Staticity/SyntaxHighlighter.cs
@@ -0,0 +1,23 @@
+﻿namespace Staticity {
+	using System;
+	using PHP.Core;
+
+	public class SyntaxHighlighter {
+		public string Highlight(string code, string language) {
+			ScriptContext context = ScriptContext.CurrentContext;
+
+			// redirect PHP output to the console:
+			context.Output = Console.Out; // Unicode text output
+			context.OutputStream = Console.OpenStandardOutput(); // byte stream output
+
+			context.Include("geshi.php", true);
+
+			var geshi = (PhpObject)context.NewObject("GeSHi", code, language);
+			var result = new PhpCallback(geshi, "parse_code").Invoke();
+			var error = new PhpCallback(geshi, "error").Invoke();
+			new PhpCallback(geshi, "enable_keyword_links").Invoke(false);
+
+			return result.ToString();
+		}
+	}
+}
